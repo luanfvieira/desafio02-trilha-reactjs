ae324b16b8ec1c581ea17c08753c5edb818f559c
“Luan Vieira” <luanfvieira@gmail.com>
2021-03-15 11:48:50 -0300

    feat: separete components to App.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,58 @@
-export function Content() {
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenreId: Number;
+  title: String;
+}
+
+export function Content(props: ContentProps) {
+  const { selectedGenreId, title } = props;
   // Complete aqui
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [selectedGenreId]);
+
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie, ind) => (
+            <MovieCard
+              key={ind}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
